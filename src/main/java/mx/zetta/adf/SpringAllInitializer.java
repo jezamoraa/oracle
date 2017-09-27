71e28062da4a6e6de2112680d58006014987fba3
Javier Zamora <je.zamoraa@outlook.com>
2017-09-27 13:32:01 -0500

    linea base (#1)

---
+++ src/main/java/mx/zetta/adf/SpringAllInitializer.java
@@ -0,0 +1,31 @@
+package mx.zetta.adf;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class SpringAllInitializer implements WebApplicationInitializer {
+
+    public void onStartup(ServletContext container) throws ServletException {
+
+        /*-final ServletRegistration.Dynamic cxf = container.addServlet("cxf", new CXFServlet());
+
+        cxf.setLoadOnStartup(1);
+        cxf.addMapping("/services/*");*/
+
+        final AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(SpringWebConfiguration.class);
+        ctx.setServletContext(container);
+
+        final ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
+
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/html4");
+
+    }
+
+}
